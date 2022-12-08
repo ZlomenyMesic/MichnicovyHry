1ff2662f961c5b1fee820664671fc34c557404f7
ZlomenyMesic <90268007+ZlomenyMesic@users.noreply.github.com>
2022-12-08 22:57:41 +0100

    Update BlockStates.cs

--- bomberman_source_code/BlockStates.cs
+++ bomberman_source_code/BlockStates.cs
@@ -16,56 +16,35 @@ namespace Bomberman
         {
             // Check if the block at the given coordinates is empty
 
-            if (Game.boardLayout[VectorMath.CalculateBoardPosition(vector)] == 0)
-            {
-                return true;
-            }
-            return false;
+            return (Game.boardLayout[VectorMath.CalculateBoardPosition(vector)] == 0);
         }
 
         public static bool IsOutOfRange(Vector2 vector)
         {
             // Check if the given coordinates are outside the board
 
-            if (((vector.X / 50) > 15) || ((vector.X / 50) < 1)
-                || ((vector.Y / 50) > 11) || ((vector.Y / 50) < 1))
-            {
-                return true;
-            }
-            return false;
+            return (((vector.X / 50) > 15) || ((vector.X / 50) < 1) || ((vector.Y / 50) > 11) || ((vector.Y / 50) < 1));
         }
 
         public static bool IsBomb(Vector2 vector)
         {
             // Check if the block at the given coordinates is a bomb
 
-            if (Game.boardLayout[VectorMath.CalculateBoardPosition(vector)] == 3)
-            {
-                return true;
-            }
-            return false;
+            return (Game.boardLayout[VectorMath.CalculateBoardPosition(vector)] == 3);
         }
 
         public static bool IsDestructable(Vector2 vector)
         {
             // Check if the block at the given coordinates is a weak brick
 
-            if (Game.boardLayout[VectorMath.CalculateBoardPosition(vector)] == 2)
-            {
-                return true;
-            }
-            return false;
+            return (Game.boardLayout[VectorMath.CalculateBoardPosition(vector)] == 2);
         }
 
         public static bool CanExplode(Vector2 vector)
         {
             // Check if the block at the given coordinates can be exploded
 
-            if (!IsOutOfRange(vector) && (IsFree(vector) || IsDestructable(vector)))
-            {
-                return true;
-            }
-            return false;
+            return (!IsOutOfRange(vector) && (IsFree(vector) || IsDestructable(vector)));
         }
 
         public static bool CanBeWalkedThrough(Vector2 vector)
@@ -73,11 +52,7 @@ namespace Bomberman
             // Check if the block at the given coordinates isn't a wall
 
             int num = Game.boardLayout[VectorMath.CalculateBoardPosition(vector)];
-            if ((num == 0) || (num == 3) || (num == 4) || (num == 5) || (num == 6))
-            {
-                return true;
-            }
-            return false;
+            return ((num == 0) || (num == 3) || (num == 4) || (num == 5) || (num == 6));
         }
         #endregion
     }
